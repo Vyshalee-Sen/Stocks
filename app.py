8df5bb5ba70881e11d848ccba4675db8146ff1ae
Monish Kumar K S <86783225+Monish3004@users.noreply.github.com>
2023-06-10 10:39:53 +0530

    Second Version

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ import plotly.express as px
 
 
 st.set_page_config(page_title="Stock Prediction", page_icon=":fire:")
+st.title("🌎 Stocks Prediction")
 
 df=pd.read_csv('GOOG.csv')
-st.dataframe(df)
 
-date=df['date']
-vol=df['volume']
+
+df['year'] = pd.to_datetime(df['date']).dt.strftime('%Y')
+
+year=st.selectbox("Select Year",options=df['year'].unique())
+df_selection=df.query("year==@year")
+st.dataframe(df_selection)
 
 #No of stocks traded
-s_traded=px.scatter(df,x="date",y=vol,animation_frame="date",range_x=["2016-01-01", "2023-04-01"],range_y=[100,14000000],title="Stocks Traded")
+s_traded=px.scatter(df_selection,x="date",y="volume",animation_frame="date",range_x=["2016-01-01", "2023-04-01"],range_y=[100,14000000],title="Stocks Traded")
 
 
-s=px.line(df,x=date,y=vol,title="Stocks Traded")
+s=px.line(df_selection,x="date",y="volume",title="Stocks Traded")
 
-
-#st.write(date_range)
 
 
 st.plotly_chart(s)
@@ -27,3 +29,4 @@ s_traded.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 80
 s_traded.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 80
 
 st.plotly_chart(s_traded)
+
