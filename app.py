0afc6e116e963b51db88a62ddf251c61b4424320
Monish Kumar K S <86783225+Monish3004@users.noreply.github.com>
2023-06-20 22:44:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,8 +19,14 @@ st.dataframe(df_selection)
 #No of stocks traded
 st.title('Stocks Traded')
 st.area_chart(df_selection,x='date',y=['volume'])
+y=str(year)
+ll="The highest volume of stocks sold in "+y+ " is "+str(df_selection['volume'].max(numeric_only = True))+" on "+str(df.loc[df['volume'] == df_selection['volume'].max(numeric_only = True), 'date'].values)
+st.write("**Insights:**")
+st.write(ll)
+tl="The lowest volume of stocks sold in "+y+ " is "+str(df_selection['volume'].min(numeric_only = True))+" on "+str(df.loc[df['volume'] == df_selection['volume'].min(numeric_only = True), 'date'].values)
+st.write(tl)
 
-s_traded=px.scatter(df_selection,x="date",y="volume",animation_frame="date",range_x=["2016-01-01", "2023-04-01"],range_y=[100,14000000],title="Stocks Traded")
+s_traded=px.scatter(df_selection,x="date",y="volume",animation_frame="date",range_x=["2016-01-01", "2023-04-01"],range_y=[100,14000000],title="Stocks Traded(Animation)")
 
 s_traded.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 80
 s_traded.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 80
@@ -30,6 +36,14 @@ st.plotly_chart(s_traded)
 #high stock price
 high=px.line(df_selection,x="date",y="high",title="High Price of Stock")
 st.plotly_chart(high)
+st.write("**Insights:**")
+ll="The stock price reached its maximum of "+ str(df_selection['high'].max(numeric_only = True))+ " in "+ y + " during the month of "+str(df.loc[df['high'] == df_selection['high'].max(numeric_only = True), 'month'].values)
+tl="The stock price reached its minimum of "+ str(df_selection['high'].min(numeric_only = True))+ " in "+ y + " during the month of "+str(df.loc[df['high'] == df_selection['high'].min(numeric_only = True), 'month'].values)
+
+st.write(ll)
+st.write(tl)
+
+
 
 #low stock price
 low=px.line(df_selection,x="date",y="low",title="Low Price of Stock")
